42d8515467bd553bda6142566db4f679ec825ad3
Galina-Goncharova <galinagoncharova1112@gmail.com>
2022-01-19 23:56:17 +0300

    task 8-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColor = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+const backgroundColor = document.querySelector('body');
+
+function changeColorRandom() {
+const color = getRandomHexColor()
+  backgroundColor.style.backgroundColor = `${color}`;
+  colorName.textContent = color;
+}
+btnChangeColor.addEventListener('click', changeColorRandom);
